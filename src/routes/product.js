33e27c7762f435a449d9e418388c651a320c357b
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-09-11 14:44:08 +0530

    minor chnages in product adding that is upload section

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,20 +2,20 @@ const express = require("express");
 const { requireSignin, adminMiddleware } = require("../common-middleware");
 const { createProduct } = require("../controller/product");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
 const shortid = require("shortid");
+const path = require('path')
 const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../uploads/");
+    cb(null,path.join(path.dirname(__dirname),'uploads'));
   },
   filename: function (req, file, cb) {
     cb(null, shortid.generate() + '-' + file.originalname);
   },
 });
 
-// var upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 router.post(
   "/product/create",
