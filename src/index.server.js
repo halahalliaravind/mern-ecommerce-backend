316be6fada6da68c0ae2f9f1b9a8e14038f6ec49
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-09-11 06:52:46 +0530

    added working create category by admin logic

--- src/index.server.js
+++ src/index.server.js
@@ -5,10 +5,9 @@ const app = express();
 const mongoose = require("mongoose");
 
 //Routes
-// const userRoutes = require('./routes/user')
-const authRoutes = require('./routes/auth')
-const adminRoutes = require('./routes/admin/auth')
-// const categoryRoute = require('./routes/category')
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin/auth");
+const categoryRoutes = require('./routes/category')
 // const productRoute = require('./routes/product')
 
 //Environment varibale
@@ -16,24 +15,24 @@ env.config();
 
 //Mongose connection method
 //mongodb+srv://root:<password>@cluster0.g6n3w.mongodb.net/<dbname>?retryWrites=true&w=majority
-mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.g6n3w.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.g6n3w.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
     {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex:true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
     }
-).then(()=>{
-    console.log('MongoDB SuccessFully Connected!!')
-});
+  )
+  .then(() => {
+    console.log("MongoDB SuccessFully Connected!!");
+  });
 
 app.use(express.json());
 //usage of routes in actually
-
-// app.use('/api',userRoutes)
-app.use('/api',authRoutes);
-app.use('/api',adminRoutes);
-// app.use('/api',categoryRoute);
+app.use("/api", authRoutes);
+app.use("/api", adminRoutes);
+app.use('/api',categoryRoutes);
 // app.use('/api',productRoute);
 
 app.listen(process.env.PORT, () => {
---
+++ src/routes/category.js
@@ -0,0 +1,23 @@
+// const express = require("express");
+// const category = require("../models/category");
+// const { addCategory, getCategories } = require("../controller/category");
+// const { requireSignin, adminMiddleware } = require("../common-middleware");
+// const router = express.Router();
+
+// router.post("/category/create", requireSignin, adminMiddleware, addCategory);
+// router.get("/category/getcategory", getCategories);
+
+// module.exports = router;
+
+const express = require("express");
+
+const category = require("../models/category");
+const { addCategory, getCategories } = require("../controller/category");
+const { requireSignin, adminMiddleware } = require("../common-middleware");
+
+const router = express.Router();
+
+router.post("/category/create", requireSignin, adminMiddleware, addCategory);
+router.get("/category/getcategory", getCategories);
+
+module.exports = router;
