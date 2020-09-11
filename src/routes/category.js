1d316e4919217b47e57a11c33637c9daeab70a12
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-09-11 07:14:01 +0530

    code refactor

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,14 +1,3 @@
-// const express = require("express");
-// const category = require("../models/category");
-// const { addCategory, getCategories } = require("../controller/category");
-// const { requireSignin, adminMiddleware } = require("../common-middleware");
-// const router = express.Router();
-
-// router.post("/category/create", requireSignin, adminMiddleware, addCategory);
-// router.get("/category/getcategory", getCategories);
-
-// module.exports = router;
-
 const express = require("express");
 
 const category = require("../models/category");
